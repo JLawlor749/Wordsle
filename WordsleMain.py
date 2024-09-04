ba637ceb8a941e4cf8ec0965f13897ae7a207c52
CaptainComedy749 <jamie.lawlor2003@gmail.com>
2024-09-04 17:49:27 +0100

    Refactoring - checking the letters of a guess is now a function, and players can also specify the length of the word they want to guess.

--- WordsleMain.py
+++ WordsleMain.py
@@ -7,15 +7,17 @@ wordnik_file = open("WordnikAPI.env", "r")
 
 wordnik_key = wordnik_file.read()
 
-wordnik_url = "http://api.wordnik.com/v4/words.json/randomWord?minCorpusCount=15&minLength=5&maxLength=5&api_key=" + wordnik_key
-
-def get_word():
+def get_word(link_address:str):
+    """
+    This function retrieves a word from the Wordnik API based on the URL provided.
+    It returns a tuple containing the word, and its length.
+    """
 
     wordnik_response = 0
 
     while wordnik_response == 0:
         try:
-            wordnik_response = urlopen(wordnik_url)
+            wordnik_response = urlopen(link_address)
         except:
             print("\nAn error has occured. Please wait...\n")
             time.sleep(12)
@@ -40,6 +42,45 @@ def get_word():
     return word_tuple
 
 
+def check_word(word:str, target_word:tuple[str, int]):
+    """
+    This function checks each letter in the guessed word against the target.
+    It prints each letter of the guess in a different colour based on correctness.
+    \nGREEN = Correct letter in the correct space.
+    \nYELLOW = Correct letter in the wrong space.
+    \nRED = Wrong letter.
+    """
+
+    goal_checklist = []
+    guess_print = []
+                    
+    for el in target_word[0]:
+        goal_checklist.append(el)
+
+    for i in range (0, target_word[1]):
+        if word[i] == (target_word[0])[i]:
+            guess_print.append(("\033[1;32;40m", i))
+            goal_checklist.remove(word[i])
+
+    for i in range (0, target_word[1]):
+        if word[i] in goal_checklist and word[i] != (target_word[0])[i] and word[i] != (target_word[0])[i]:
+            guess_print.append(("\033[1;33;40m", i))
+            goal_checklist.remove(word[i])
+
+    for i in range (0, target_word[1]):
+        if word[i] not in goal_checklist and ("\033[1;33;40m", i) not in guess_print and ("\033[1;32;40m", i) not in guess_print:
+            guess_print.append(("\033[1;31;40m", i))
+
+    guess_print.sort(key=lambda tup:tup[1])
+
+    for el in guess_print:
+        print(el[0] + word[el[1]], end="")
+
+    print("\033[0;37;40m")
+    print("")
+
+
+
 exit_game = False
 
 print("\n        ~ Welcome to Wordsle! ~")
@@ -52,26 +93,31 @@ while exit_game == False:
     print("3. Quit\n")
 
     selection = input()
+    print("")
 
     match selection:
         case "1":
+
+            print("Please input the length of the word you want to guess:")
+            desired_length = int(input())
+            print("")
             
-            puzzle_goal = get_word()
+            wordnik_url = "http://api.wordnik.com/v4/words.json/randomWord?minCorpusCount=20&minLength=" + str(desired_length) + "&maxLength=" + str(desired_length) + "&api_key=" + wordnik_key
+            puzzle_goal = get_word(wordnik_url)
             
-            print("Input your guesses below:\n")
+            print("Input your guesses below:")
 
             lost = False
             won = False
             letters_wrong = []
-            guesses = 5
-
-            #print(puzzle_goal[0])
+            guesses = puzzle_goal[1] + 1
 
             while lost == False and won == False:
                 for i in range (0, puzzle_goal[1]):
                     print("-", end = "")
 
                 print("")
+
                 guess = ""
                 while len(guess) != puzzle_goal[1]:
                     guess = input()
@@ -86,41 +132,13 @@ while exit_game == False:
 
                 else:
                     guesses -= 1
+                    check_word(guess, puzzle_goal)
+                    print(str(guesses) + " guesses remaining.")
 
-                    goal_checklist = []
-                    guess_print = []
                     
-                    for el in puzzle_goal[0]:
-                        goal_checklist.append(el)
-
-                    for i in range (0, puzzle_goal[1]):
-                        if guess[i] == (puzzle_goal[0])[i]:
-                            guess_print.append(("\033[1;32;40m", i))
-                            goal_checklist.remove(guess[i])
-
-                    for i in range (0, puzzle_goal[1]):
-                        if guess[i] in goal_checklist and guess[i] != (puzzle_goal[0])[i] and guess[i] != (puzzle_goal[0])[i]:
-                            guess_print.append(("\033[1;33;40m", i))
-                            goal_checklist.remove(guess[i])
-
-                    for i in range (0, puzzle_goal[1]):
-                        if guess[i] not in goal_checklist and ("\033[1;33;40m", i) not in guess_print and ("\033[1;32;40m", i) not in guess_print:
-                            guess_print.append(("\033[1;31;40m", i))
-
-                    guess_print.sort(key=lambda tup:tup[1])
-
-                    for el in guess_print:
-                        print(el[0] + guess[el[1]], end="")
-
-                    print("\033[0;37;40m")
-                    print("")
-                    
-
                 if guesses < 1:
                     lost = True
                     print("\nUh Oh! You lose! The answer was {}.\n".format(puzzle_goal[0]))
-
-
 
 
         case "3":
